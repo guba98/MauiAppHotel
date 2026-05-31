8de4c770441ef004991e5e033ae31d2339b09eed
Guba <guba9779@gmail.com>
2026-05-31 11:19:14 -0300

    App funcionando ate a parte do valor

--- MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
+++ MauiAppHotel/Views/ContratacaoHospedagem.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel.Views;
 
 public partial class ContratacaoHospedagem : ContentPage
@@ -30,17 +32,28 @@ public partial class ContratacaoHospedagem : ContentPage
 		}
 	}
 
-	private void Button_Clicked_1(object sender, EventArgs e)
-	{
-		try
-		{
-			Navigation.PushAsync(new HospedagemContratada());
-		}
-		catch (Exception ex)
-		{
-			DisplayAlert("Ocorreu um erro", ex.Message, "OK");
-		}
-	}
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        try
+        {
+            Hospedagem h = new Hospedagem
+            {
+                QuartoSelecionado = (Quarto)pck_quarto.SelectedItem,
+                QntAdultos = Convert.ToInt32(stp_adultos.Value),
+                QntCriancas = Convert.ToInt32(stp_criancas.Value),
+                DataCheckIn = dtpck_checkin.Date,
+                DataCheckOut = dtpck_checkout.Date,
+            };
+            await Navigation.PushAsync(new HospedagemContratada()
+            {
+                BindingContext = h
+            });
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 
     private void dtpck_checkin_DateSelected(object sender, DateChangedEventArgs e)
     {
@@ -51,4 +64,5 @@ public partial class ContratacaoHospedagem : ContentPage
 		dtpck_checkout.MinimumDate = data_selecionada_checkin.AddDays(1);
 		dtpck_checkout.MaximumDate = data_selecionada_checkin.AddMonths(2);
     }
+
 }
